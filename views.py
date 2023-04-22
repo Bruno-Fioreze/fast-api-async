4ccab83646563a15f8fca68b8ea0be84b08ebad4
Bruno Fioreze <brunofioreze@pop-os.localdomain>
2023-04-22 13:15:05 -0300

    added delete user in views

--- views.py
+++ views.py
@@ -12,3 +12,11 @@ async def create_user(user_input: UserCreateInput):
         return Standard(message="OK")
     except Exception as e:
         raise HTTPException(400, detail=str(e))
+
+@user_router.delete("/delete/{pk}", response_model=Standard, responses={400: {"model": ErrorOutput}})
+async def delete_user(pk: int):
+    try: 
+        await UserService.delete(pk=pk)
+        return Standard(message="OK")
+    except Exception as e:
+        raise HTTPException(400, detail=str(e))
