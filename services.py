b1a39b604153f3b49ecac2802b639472ab73145b
Bruno Fioreze <brunofioreze@pop-os.localdomain>
2023-04-22 13:11:29 -0300

    created service delete user

--- services.py
+++ services.py
@@ -1,5 +1,6 @@
 from db.postgres import models
 from db.postgres.connection import async_session
+from sqlalchemy import delete
 
 class UserService:
     async def create(name):
@@ -7,3 +8,9 @@ class UserService:
             user = models.User(name=name)
             session.add(user)
             await session.commit()
+            
+    async def delete(pk):
+        async with async_session() as session:
+            await session.execute(delete(models.User).where(models.User.id==pk))
+            await session.commit()
+            
